a6450612c74ed9393713ce3c134c4c81025fd1cc
Glyn Davies <glynwdavies@gmail.com>
2026-05-23 08:49:49 +0100

    Created members controller.

---
+++ Dating.API/Controllers/MembersController.cs
@@ -0,0 +1,19 @@
+﻿using Dating.Domain.Entities;
+using Dating.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MembersController(DatingSQLDBContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<User>>> ListAll()
+    {
+        var members = await context.Users.ToListAsync();
+
+        return members;
+    }
+}
