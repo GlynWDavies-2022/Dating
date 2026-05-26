c279836e5f91d006a98aa590396ec591fd9bacf7
Glyn Davies <glynwdavies@gmail.com>
2026-05-26 20:40:12 +0100

    Added PasswordHash and PasswordSalt columns to User table.

--- Dating.Domain/Entities/User.cs
+++ Dating.Domain/Entities/User.cs
@@ -4,4 +4,6 @@ public class User : BaseEntity
 {
     public required string DisplayName { get; set; }
     public required string Email { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
