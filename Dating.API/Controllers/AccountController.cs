36967ceb47c32022ab3f8ddc55b071e7493c2db1
Glyn Davies <glynwdavies@gmail.com>
2026-05-27 07:47:58 +0100

    Added RegisterDTO.

--- Dating.API/Controllers/AccountController.cs
+++ Dating.API/Controllers/AccountController.cs
@@ -1,34 +1,47 @@
-﻿using Dating.Domain.Entities;
+﻿using Dating.Application.DTO;
+using Dating.Domain.Entities;
 using Dating.Infrastructure.Database;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Dating.API.Controllers
+namespace Dating.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController(DatingSQLDBContext context) : BaseApiController
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController(DatingSQLDBContext context) : BaseApiController
+    [HttpPost("register")]
+    public async Task<ActionResult<User>> Register(RegisterDTO registerDTO)
     {
-        [HttpPost("register")]
-        public async Task<ActionResult<User>> Register(string email, string displayName, string password)
+        if (await EmailExists(registerDTO.Email))
         {
-            using var hmac = new HMACSHA512();
-
-            var user = new User
-            {
-                Email = email,
-                DisplayName = displayName,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-                PasswordSalt = hmac.Key
-            };
-
-            context.Users.Add(user);
-
-            await context.SaveChangesAsync();
-
-            return Ok(user);
+            return BadRequest("Email has already been used.");
         }
 
+        using var hmac = new HMACSHA512();
+
+        var user = new User
+        {
+            Email = registerDTO.Email,
+            DisplayName = registerDTO.DisplayName,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
+            PasswordSalt = hmac.Key
+        };
+
+        context.Users.Add(user);
+
+        await context.SaveChangesAsync();
+
+        return Ok(user);
     }
+
+    private async Task<bool> EmailExists(string email)
+    {
+        return await context.Users.AnyAsync(user => user.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+    }
+
 }
+
+
