0722405ea195ae227d13d9c33e3b43544e2e42af
Glyn Davies <glynwdavies@gmail.com>
2026-05-27 07:11:22 +0100

    Created new AccountController.

---
+++ Dating.API/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Dating.Domain.Entities;
+using Dating.Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Dating.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(DatingSQLDBContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(string email, string displayName, string password)
+        {
+            using var hmac = new HMACSHA512();
+
+            var user = new User
+            {
+                Email = email,
+                DisplayName = displayName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
+            };
+
+            context.Users.Add(user);
+
+            await context.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+    }
+}
