7f2701ab7852b5dacb8f14085d6a4f2794aace2f
Glyn Davies <glynwdavies@gmail.com>
2026-06-09 20:54:26 +0100

    Implemented login endpoint.

--- Dating.API/Controllers/AccountController.cs
+++ Dating.API/Controllers/AccountController.cs
@@ -35,6 +35,31 @@ public class AccountController(DatingSQLDBContext context) : BaseApiController
         return Ok(user);
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult> Login([FromBody] LoginDTO loginDTO)
+    {
+        var user = await context.Users.SingleOrDefaultAsync(u => u.Email.Equals(loginDTO.Email));
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid email.");
+        }
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid password.");
+            }
+        }
+
+        return Ok();
+    }
+
     private async Task<bool> EmailExists(string email)
     {
         return await context.Users.AnyAsync(user => EF.Functions.Like(user.Email,email));
