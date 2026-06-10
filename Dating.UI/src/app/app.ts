b7adab67c868fbe5917ca457e1401cc1479d8c82
Glyn Davies <glynwdavies@gmail.com>
2026-06-10 21:15:05 +0100

    Created nav component.

--- Dating.UI/src/app/app.ts
+++ Dating.UI/src/app/app.ts
@@ -1,9 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from '../layout/nav/nav';
 
 @Component({
   selector: 'app-root',
+  imports: [Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
