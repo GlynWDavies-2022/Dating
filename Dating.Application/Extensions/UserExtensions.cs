64a831ef9ae144d14755eddb6d63a33cc5a78572
Glyn Davies <glynwdavies@gmail.com>
2026-06-10 09:28:19 +0100

    Progressed UserExtensions class.

---
+++ Dating.Application/Extensions/UserExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Dating.Application.DTO;
+using Dating.Domain.Entities;
+
+namespace Dating.Application.Extensions;
+
+public static class UserExtensions
+{
+    public static UserDTO ToDTO(this User user)
+    {
+        // Todo...
+
+        return null!;
+    }
+}
