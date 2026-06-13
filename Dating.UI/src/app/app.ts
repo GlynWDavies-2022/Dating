b1028afa07ee75169508a5d97b01da249c4fe8ff
Glyn Davies <glynwdavies@gmail.com>
2026-06-13 17:36:29 +0100

    Implemented parent to child communication.

--- Dating.UI/src/app/app.ts
+++ Dating.UI/src/app/app.ts
@@ -4,6 +4,7 @@ import { lastValueFrom } from 'rxjs';
 import { Nav } from '../layout/nav/nav';
 import { AccountService } from '../core/services/account-service';
 import { Home } from '../features/home/home';
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
@@ -19,7 +20,7 @@ export class App implements OnInit {
 
   protected readonly title = 'Dating Application';
 
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   async ngOnInit() {
 
@@ -41,7 +42,7 @@ export class App implements OnInit {
 
   getMembers() {
     try {
-      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
     } catch (error) {
       console.log(error);
       throw error;
--- Dating.UI/src/core/services/account-service.ts
+++ Dating.UI/src/core/services/account-service.ts
@@ -29,7 +29,9 @@ export class AccountService {
   }
 
   logout() {
+
     localStorage.removeItem('user');
+
     this.currentUser.set(null);
   }
 
