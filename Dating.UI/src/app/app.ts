dce541767372dbbea12cc205fc404e134f93f355
Glyn Davies <glynwdavies@gmail.com>
2026-06-19 07:25:55 +0100

    Refactored.

--- Dating.UI/src/app/app.ts
+++ Dating.UI/src/app/app.ts
@@ -1,11 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from '../layout/nav/nav';
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 import { Router, RouterOutlet } from '@angular/router';
-import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -13,36 +8,8 @@ import { NgClass } from '@angular/common';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-
-  private accountService = inject(AccountService);
+export class App {
 
   protected router = inject(Router);
 
-  private http = inject(HttpClient);
-
-  protected readonly title = 'Dating Application';
-
-  protected members = signal<User[]>([]);
-
-  async ngOnInit() {
-
-    this.members.set(await this.getMembers());
-
-    this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-
-    
-  }
-
-  getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-  }
 }
